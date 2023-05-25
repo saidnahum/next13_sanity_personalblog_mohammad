4c7da79a2009983b74440076e9f9a710862cd864
Said Nahúm <saidnahum@gmail.com>
2023-05-25 13:53:21 -0600

    First Commit: next13 + Sanity + Google Fonts

---
+++ app/fonts.copy.ts
@@ -0,0 +1,19 @@
+// import { Poppins, Montserrat, Pangolin } from 'next/font/google';
+
+// export const popins = Poppins({
+//   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+//   subsets: ['latin'],
+//   variable: '--font-popins'
+// });
+
+// export const montserrat = Montserrat({
+//   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+//   subsets: ['latin', 'latin-ext', 'cyrillic'],
+//   variable: '--font-montserrat'
+// });
+
+// export const pangolin = Pangolin({
+//   subsets: ['latin'],
+//   weight: '400',
+//   variable: '--font-pangolin',
+// });
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        pangolin: ['var(--font-pangolin)'],
+        bodyFont: ['var(--font-popins)'],
+        titleFont: ['var(--font-montserrat)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primaryColor: "#111111",
+        secondaryColor: "#9b59b6",
+        bgColor: "#34495e",
       },
     },
   },
